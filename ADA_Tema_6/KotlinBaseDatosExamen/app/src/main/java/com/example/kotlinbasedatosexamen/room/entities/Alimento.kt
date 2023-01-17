c8abef5433c911712bd058a272b6c87d903e3285
ivanfm-JulioVerne <ivan.fernandez.mendez.alu@iesjulioverne.es>
2023-01-17 18:00:34 +0100

    commit de prueba

--- ADA_Tema_6/KotlinBaseDatosExamen/app/src/main/java/com/example/kotlinbasedatosexamen/room/entities/Alimento.kt
+++ ADA_Tema_6/KotlinBaseDatosExamen/app/src/main/java/com/example/kotlinbasedatosexamen/room/entities/Alimento.kt
@@ -1,8 +1,14 @@
 package com.example.kotlinbasedatosexamen.room.entities
 
 data class Alimento(
-    var id_alimento,
+    var id_alimento:Int?,
+    var nombre:String,
+    var proteinas:Int,
+    var grasas:Int,
+    var hidratos:Int
 
 ){
+    cosntructor (nombre:String,proteinas:Int,grasas:Int,hidratos:Int):this(id_alimento=){
 
+    }
 }
